49b281254e994cd8439247f2057fe2b68c006a73
yamashitatubasa <k021g1247@g.neec.ac.jp>
2024-09-13 11:53:33 +0900

    修正

--- GameScene.cpp
+++ GameScene.cpp
@@ -83,7 +83,7 @@ void GameScene::Reset()
 			if (map->GetBlockNum(y, x) != 0)
 			{
 				//位置、サイズ、種類を格納
-				player_->SetBlockPos(Vector2{ map->GetBlockPosition(y, x).x_,map->GetBlockPosition(y, x).y_ }, Vector2{ (float)blockSize ,(float)blockSize }, map->GetBlockNum(y, x));
+				player_->SetBlockPos(Vector2{ map->GetBlockPosition(0, y, x).x_,map->GetBlockPosition(0, y, x).y_ }, Vector2{ (float)blockSize ,(float)blockSize }, map->GetBlockNum(y, x));
 			}
 		}
 	}
